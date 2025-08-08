dfa9abce73eba78b0f4aabd6d13d45a079508302
Rasolomanana Olivier <herimanitraolivierr@gmail.com>
2025-08-08 19:54:18 +0300

    feat: add drizzle configuration and update database schema with auth support

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "drizzle-kit";
+
+export default {
+  schema: "./lib/db/schema.ts",
+  out: "./lib/db/migrations",
+  dialect: "postgresql",
+  schemaFilter: ["public"],
+  dbCredentials: {
+    url: process.env.POSTGRES_URL!,
+  },
+} satisfies Config;
